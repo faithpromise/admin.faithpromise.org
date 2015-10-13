519da86db115b46b831731e72e9b54661138c121
Brad Roberts <brad@bradroberts.net>
2015-10-13 14:14:54 -0400

    Added settings for less task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@ module.exports = function (grunt) {
 
     var config = {};
 
+    grunt.option('less', {
+        root: 'less',
+        src: 'less/admin.less',
+        dest_dev: 'public/build/admin.dev.css',
+        dest_production: 'public/build/admin.min.css'
+    });
+
     // Load NPM tasks
     require('load-grunt-tasks')(grunt);
 
