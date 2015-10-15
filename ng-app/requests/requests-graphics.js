72c959375cb7e54e39285ec69d51b067c8611b9f
Brad Roberts <brad@bradroberts.net>
2015-10-15 17:06:50 -0400

    Working on graphics request form

--- ng-app/requests/requests-graphics.js
+++ ng-app/requests/requests-graphics.js
@@ -19,8 +19,35 @@
     function Controller() {
 
         var vm = this;
+        vm.toggle_item = toggle_item;
+        vm.is_item_selected = is_item_selected;
+
+        vm.items = [
+            {slug: 'invite_card', title: 'Invite Card'},
+            {slug: 'flyer', title: 'Flyer'},
+            {slug: 't_shirt', title: 'T-Shirt'},
+            {slug: 'sign', title: 'Sign'},
+            {slug: 'logo', title: 'Logo'},
+            {slug: 'other', title: 'Other'}
+        ];
+
+        vm.selected_items = {};
 
         init();
+
+        function toggle_item(item) {
+
+            if (is_item_selected(item.slug)) {
+                delete vm.selected_items[item.slug];
+            } else {
+                vm.selected_items[item.slug] = { active: true };
+            }
+        }
+
+        function is_item_selected(slug) {
+            var result = vm.selected_items.hasOwnProperty(slug) && vm.selected_items[slug].active;
+            return result;
+        }
 
         function init() {
 
