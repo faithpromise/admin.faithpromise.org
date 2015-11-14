9cc0960f39d6ac9b01de34740eb2f10db84b24ce
Brad Roberts <email@bradroberts.net>
2015-11-14 11:11:05 -0500

    Added JWT route middleware and cleaned up file

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,8 +4,7 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel
-{
+class Kernel extends HttpKernel {
     /**
      * The application's global HTTP middleware stack.
      *
@@ -13,11 +12,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-//        \App\Http\Middleware\EncryptCookies::class,
-//        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-//        \Illuminate\Session\Middleware\StartSession::class,
-//        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-//        \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
@@ -26,8 +20,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'jwt.auth'    => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
