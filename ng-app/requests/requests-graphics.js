43ec40c38600c35b37ab491717c39c7e6ef980bf
Brad Roberts <brad@bradroberts.net>
2015-11-18 17:19:39 -0500

    Refactoring

--- ng-app/requests/requests-graphics.js
+++ ng-app/requests/requests-graphics.js
@@ -14,9 +14,9 @@
         };
     }
 
-    Controller.$inject = ['$scope', '$rootScope'];
+    Controller.$inject = ['$scope', '$rootScope', 'requestsService'];
 
-    function Controller($scope, $rootScope) {
+    function Controller($scope, $rootScope, requestsService) {
 
         var vm = this;
         vm.user = $rootScope.user;
@@ -27,15 +27,14 @@
         vm.submit = submit;
         vm.helper_dates = [];
         vm.items = {
-            invite_card: {title: 'Invite Card'},
-            flyer: {title: 'Flyer'},
-            seat_card: {title: 'Seatback Card'},
-            slide: {title: 'Slide In Service'},
-            t_shirt: {title: 'T-Shirt'},
-            sign: {title: 'Sign'},
-            logo: {title: 'Logo'},
-            website_update: {title: 'Website Update'},
-            other: {title: 'Other'}
+            invite_card: {title: 'Invite Card', description_label: 'What should your invite card say?'},
+            flyer: {title: 'Flyer', description_label: 'What should it say?'},
+            seat_card: {title: 'Seatback Card', description_label: 'Briefly describe the purpose of your card?'},
+            slide: {title: 'Slide In Service', description_label: 'What should your slide say?'},
+            t_shirt: {title: 'T-Shirt', description_label: 'Briefly describe your t-shirt idea.'},
+            sign: {title: 'Sign', description_label: 'What should your sign say?'},
+            website_update: {title: 'Website Update', description_label: 'What needs to be added or changed on the website?'},
+            other: {title: 'Other Project', description_label: 'Please describe your project?'}
         };
 
         function submit() {
@@ -43,17 +42,18 @@
                 requests: gather_requests()
             };
 
-            console.log(data);
+            requestsService.batch_save(data).then(function (response) {
+                console.log('requests sent', response, data);
+            });
         }
 
         function gather_requests() {
             var result = [];
 
-            angular.forEach(vm.items, function(item, key) {
+            angular.forEach(vm.items, function (item, key) {
                 if (item.selected) {
                     item.type = key;
                     item.subject = vm.subject;
-                    item.description = 'Event is happening on ' + vm.event_date;
                     result.push(item);
                 }
             });
