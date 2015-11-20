1ffdb4310e5e5cd28d2ae690d8b06a35d80bc6b0
Brad Roberts <brad@bradroberts.net>
2015-11-20 15:07:09 -0500

    Updated middleware to use Staff model for zendesk id rather than User

--- app/Http/Middleware/ZendeskUserId.php
+++ app/Http/Middleware/ZendeskUserId.php
@@ -15,32 +15,32 @@ class ZendeskUserId {
      */
     public function handle($request, Closure $next) {
 
-        $user = $request->user();
+        $staffer = $request->user()->Staff;
 
-        if (!$user->zendesk_user_id) {
+        if (!$staffer->zendesk_user_id) {
 
             // Search for user
-            $zendesk_user_search = Zendesk::users()->search(['query' => $user->email]);
+            $zendesk_user_search = Zendesk::users()->search(['query' => $staffer->email]);
 
             // Assign zendesk id
             if (count($zendesk_user_search->users)) {
 
-                $user->Staff->zendesk_user_id = $zendesk_user_search->users[0]->id;
+                $staffer->zendesk_user_id = $zendesk_user_search->users[0]->id;
 
-            // Create zendesk user and assign id
+                // Create zendesk user and assign id
             } else {
 
                 $response = Zendesk::users()->create([
-                    'name'     => $user->name,
-                    'email'    => $user->email,
+                    'name'     => $staffer->name,
+                    'email'    => $staffer->email,
                     'verified' => true
                 ]);
 
-                $user->Staff->zendesk_user_id = $response->user->id;
+                $staffer->zendesk_user_id = $response->user->id;
 
             }
 
-            $user->Staff->save();
+            $staffer->save();
 
         }
 
