1a1dde25543c6bb884d40e8b87cdd0ef75231a6c
Brad Roberts <brad@bradroberts.net>
2015-12-08 14:47:48 -0500

    Added docblock

--- app/FaithPromise/Zendesk/TicketFactory.php
+++ app/FaithPromise/Zendesk/TicketFactory.php
@@ -4,6 +4,13 @@ namespace App\FaithPromise\Zendesk;
 
 class TicketFactory {
 
+    /**
+     * @param $type
+     * @param $ticket
+     * @param $user
+     * @return Ticket
+     * @throws \Exception
+     */
     public static function create($type, $ticket, $user) {
 
         $class_name = 'App\\Faithpromise\\Zendesk\\TicketTypes\\' . studly_case($type);
