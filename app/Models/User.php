3a4b452cd91b16c1cb245f7bf7ed31214686acef
Brad Roberts <brad@bradroberts.net>
2015-12-10 09:55:23 -0500

    Updated User model with first and last name properties. Image now defaults to gravatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,28 +2,37 @@
 
 namespace App\Models;
 
+use FaithPromise\Shared\Models\Staff;
 use FaithPromise\Shared\Models\User as BaseUser;
 
 class User extends BaseUser {
 
-    protected $table = 'users';
-    protected $fillable = ['name', 'email', 'password', 'fellowship_one_user_id'];
-    protected $appends = ['thumb', 'first_name', 'last_name'];
+    protected $fillable = ['first_name', 'last_name', 'name', 'email', 'password', 'fellowship_one_user_id'];
+    protected $appends = ['thumb'];
 
     public function staff() {
-        return $this->hasOne('FaithPromise\Shared\Models\Staff', 'email', 'email');
+        return $this->hasOne(Staff::class, 'email', 'email');
     }
 
     public function getThumbAttribute() {
-        return image_url('sm', 'quarter', $this->Staff->image);
+
+        if ($this->Staff) {
+            return image_url('sm', 'quarter', $this->Staff->image);
+        }
+
+        return "http://www.gravatar.com/avatar/" . md5(strtolower(trim($this->email))) . "?s=200";
+    }
+
+    public function getNameAttribute() {
+        return $this->first_name . ' ' . $this->last_name;
     }
 
     public function getFirstNameAttribute() {
-        return $this->Staff->first_name;
+        return $this->Staff ? $this->Staff->first_name : $this->getOriginal('first_name');
     }
 
     public function getLastNameAttribute() {
-        return $this->Staff->last_name;
+        return $this->Staff ? $this->Staff->last_name : $this->getOriginal('last_name');
     }
 
     public function toClient() {
