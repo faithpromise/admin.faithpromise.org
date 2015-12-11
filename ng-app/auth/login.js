362c68d2f233bdd9eb38212a67be4a41f1473e72
Brad Roberts <brad@bradroberts.net>
2015-12-11 16:32:58 -0500

    Updated login link to hit the server

--- ng-app/auth/login.js
+++ ng-app/auth/login.js
@@ -6,42 +6,8 @@
     function directive() {
         return {
             templateUrl: '/build/ng-app/auth/login.html',
-            restrict: 'E',
-            controller: Controller,
-            controllerAs: 'vm',
-            bindToController: true,
-            scope: {}
+            restrict:    'E'
         };
-    }
-
-    Controller.$inject = ['$window', '$location', '$rootScope', '$auth'];
-
-    function Controller($window, $location, $rootScope, $auth) {
-
-        var vm = this;
-
-        vm.login = login;
-
-        function login() {
-
-            $auth.authenticate('faithpromise').then(function (response) {
-
-                if (response.data.user) {
-                    $window.localStorage.user = JSON.stringify(response.data.user);
-                    $rootScope.user = response.data.user;
-                }
-
-                $location.path('/');
-
-            }).catch(function (response) {
-
-                alert('An error occurred. Check console');
-                console.log('error', response);
-
-            });
-
-        }
-
     }
 
 })(angular.module('admin'));
