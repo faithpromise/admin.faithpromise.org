c79275f1d71ef51dfb2b742d7f63df8a7b2024f5
Brad Roberts <brad@bradroberts.net>
2015-12-11 16:30:24 -0500

    Added error method to register request

--- ng-app/auth/register.js
+++ ng-app/auth/register.js
@@ -5,12 +5,12 @@
 
     function directive() {
         return {
-            templateUrl: '/build/ng-app/auth/register.html',
-            restrict: 'E',
-            controller: Controller,
-            controllerAs: 'vm',
+            templateUrl:      '/build/ng-app/auth/register.html',
+            restrict:         'E',
+            controller:       Controller,
+            controllerAs:     'vm',
             bindToController: true,
-            scope: {}
+            scope:            {}
         };
     }
 
@@ -18,18 +18,22 @@
 
     function Controller($http) {
 
-        var vm = this;
+        var vm            = this;
         vm.email_username = null;
-        vm.email_sent = false;
-        vm.send = send;
+        vm.email_sent     = false;
+        vm.send           = send;
 
         function send() {
 
             var data = { username: vm.email_username };
 
-            $http.post('/auth/register', data).then(function(response) {
-                vm.email_sent = true;
-            });
+            $http.post('/auth/register', data)
+                .success(function () {
+                    vm.email_sent = true;
+                })
+                .error(function () {
+                    alert('Error occurred')
+                });
 
         }
 
