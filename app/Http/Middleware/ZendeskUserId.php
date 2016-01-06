44f70fc1b7a4b50384878a547c8129980ebfe402
Brad Roberts <brad@bradroberts.net>
2016-01-06 14:12:31 -0500

    Added type hint

--- app/Http/Middleware/ZendeskUserId.php
+++ app/Http/Middleware/ZendeskUserId.php
@@ -4,16 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Huddle\Zendesk\Facades\Zendesk;
+use Illuminate\Http\Request;
 
 class ZendeskUserId {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param  Request $request
      * @param  \Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next) {
+    public function handle(Request $request, Closure $next) {
 
         $user = $request->user();
 
